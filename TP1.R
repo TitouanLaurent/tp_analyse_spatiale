6411ba1b1cdc13fc9e0ce2fa88e471099275e371
tlaurent <titouan.laurent@eleve.ensai.fr>
2024-02-08 09:02:17 +0000

    fin cours 1

--- TP1.R
+++ TP1.R
@@ -50,4 +50,79 @@ commune_Bretagne$surf2 <- set_units(commune_Bretagne$surf2, km^2)
 
 
 # Question 12
-dept_bretagne
+dept_bretagne <- commune_Bretagne %>%
+  group_by(dep) %>% 
+  summarise(surf2 = sum(surf2))
+plot(dept_bretagne)
+
+
+# Question 13
+depart_bretagne <- commune_Bretagne %>%
+  group_by(dep) %>% 
+  summarise(geometry = st_union(geometry))
+plot(depart_bretagne)
+
+
+# Question 14
+centroid_depart_bretagne <- st_centroid(depart_bretagne)
+centroid_depart_bretagne <- st_sf(geometry = centroid_depart_bretagne)
+print(centroid_depart_bretagne)
+
+##a => type point
+
+##b
+plot(st_geometry(depart_bretagne))
+plot(st_geometry(centroid_depart_bretagne), add = TRUE, col = "red")
+
+##c
+depart_names <- data.frame(dep = unique(depart_bretagne$dep),
+                         dept_lib = c("Côtes d'Armor", "Finistère", "Île-et-Vilaine", "Morbihan"))
+centroid_depart_bretagne <- merge(centroid_depart_bretagne, depart_names, by = "dep", all.x = TRUE)
+
+##d
+centroid_coords <- st_coordinates(centroid_depart_bretagne)
+centroid_coords <- bind_cols(centroid_coords, centroid_depart_bretagne[, c("dep", "dept_lib")])
+centroid_coords <- st_drop_geometry(centroid_coords)
+
+
+##e
+plot(st_geometry(depart_bretagne))
+plot(st_geometry(centroid_depart_bretagne), add = TRUE, col = "red")
+text(x = centroid_coords[, "X"], 
+     y = centroid_coords[, "Y"], 
+     labels = centroid_depart_bretagne$dept_lib, 
+     pos = 1)
+
+
+# Question 15
+intersection <- st_intersects(centroid_depart_bretagne, commune)
+commune %>%
+  slice(unlist(intersection))
+
+
+# Question 16
+inter <- st_intersection(centroid_depart_bretagne, commune)
+str(inter)
+inter %>%
+  select(libelle)
+
+
+# Question 17
+chefs_lieux <- data.frame(dep = c("22", "29", "35", "56"),
+                          libelle = c("Saint-Brieuc", "Quimper", "Rennes", "Vannes"))
+chefs_lieux <- merge(chefs_lieux, commune, by = "libelle", all.x = TRUE)
+
+chef_lieux_coords <- st_coordinates(chefs_lieux)
+distances <- st_distance(centroid_coords, chef_lieux_coords)
+
+
+
+
+
+
+
+
+
+
+
+
